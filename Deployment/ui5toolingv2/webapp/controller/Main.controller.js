23ebe2216c8bec8b05a201a2999232126d4559b7
Ashish Ahire <ashish.ahire@in.ibm.com>
2020-08-20 16:10:33 +0530

    ui5tooling v2 to build run and deploy

---
+++ Deployment/ui5toolingv2/webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("view.ui5toolingv2.controller.Main", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ Deployment/ui5toolingv2/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"view/ui5toolingv2/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
